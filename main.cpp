952eee7127b85bb54e4ed3557519a382cfa3b19b
tlstommy <tls.tommy11@gmail.com>
2023-09-14 11:43:04 -0400

    setup wsl

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+//g++ -Wall -Wextra -o main main.cpp && ./main
+
+int main(){
+
+    printf("hi\n");
+
+    return 0;
+}
